7739ac8d7bef571c3c97979d71c6dafd7b50350b
allisteraniceto <allisteraniceto@gmail.com>
2024-01-03 18:39:51 -0800

    initialized navbar component and styling. created components and pages folder

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+export default function NavBar() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>hello 1</li>
+                    <li>hello 2</li>
+                    <li>hello 3</li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
