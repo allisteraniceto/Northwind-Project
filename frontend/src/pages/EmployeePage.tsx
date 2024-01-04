21b1d8aa65c50fbdbf2430476c2c6f7a36dd6c8a
allisteraniceto <allisteraniceto@gmail.com>
2024-01-03 21:11:54 -0800

    added nopage, manager, hr, employee pages and navbar

---
+++ frontend/src/pages/EmployeePage.tsx
@@ -0,0 +1,14 @@
+//Employee Dashboard
+
+import NavBar from "../components/NavBar";
+
+function EmployeePage() {
+	return (
+		<>
+			<h2>Employee Dashboard</h2>
+			<NavBar />
+		</>
+	);
+}
+
+export default EmployeePage;
---
+++ frontend/src/pages/HrPage.tsx
@@ -0,0 +1,14 @@
+//HR Dashboard
+
+import NavBar from "../components/NavBar";
+
+function HrPage() {
+	return (
+		<>
+			<h2>HR Dashboard</h2>
+			<NavBar />
+		</>
+	);
+}
+
+export default HrPage;
