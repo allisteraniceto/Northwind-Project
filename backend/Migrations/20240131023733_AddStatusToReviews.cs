b7292398994e122585c91619697a1bc61e355abb
Mark Kneblik <markkneblik@gmail.com>
2024-01-31 20:09:37 -0800

    added submit button to submission form and created migration to change number of chars Status column can hold

---
+++ backend/Migrations/20240131023733_AddStatusToReviews.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    public partial class AddStatusToReviews : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Reviews",
+                type: "nvarchar(25)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Reviews",
+                type: "nvarchar(15)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)");
+        }
+    }
+}
