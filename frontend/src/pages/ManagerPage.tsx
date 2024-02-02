ced4ce20e95a135fca492dda768da18350112979
Daniel <99993616+daniel4261@users.noreply.github.com>
2024-02-02 09:33:24 -0800

    Add files via upload

--- frontend/src/pages/ManagerPage.tsx
+++ frontend/src/pages/ManagerPage.tsx
@@ -1,21 +1,26 @@
 //Manager Dashboard
 //Employee Dashboard
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import "../styles/InteractionsPane.css";
 import EmployeeList from "../components/EmployeeList";
+import StatusPane from "../components/StatusPane";
 
 function ManagerPage() {
   return (
     <>
       <Header dashboard="Manager" />
       <EmployeeList />
-      <div className='employee-container'>
+      <div className="employee-container">
         <Link to="/ManagerReviewForm">
-          <button type="button" className='performance-review-button'>
+          <button type="button" className="performance-review-button">
             Take Me To This Employee's Performance Review
           </button>
         </Link>
+        <StatusPane
+          completed={false}
+          managerPicture={"../assets/complete.png"}
+        />
       </div>
     </>
   );
