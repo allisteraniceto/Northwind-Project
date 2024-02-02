2239644b2da17cc6a4fdd8dcd741bcef2a34f42a
Daniel <99993616+daniel4261@users.noreply.github.com>
2024-02-02 09:16:22 -0800

    Add files via upload

--- frontend/src/components/StatusPane.tsx
+++ frontend/src/components/StatusPane.tsx
@@ -1,15 +1,28 @@
 import "../styles/StatusPane.css";
+import complete from "../assets/complete.png"; //../assets/complete.png
+import uncomplete from "../assets/uncomplete.png";
 
-interface Props {
-  employeePicture: string;
+function sendReminder() {
+  return;
 }
 
-export default function StatusPane({ employeePicture }: Props) {
+interface Props {
+  completed: boolean;
+  managerPicture: string;
+}
+
+export default function StatusPane({ completed, managerPicture }: Props) {
   return (
     <>
-      <nav className="has-form-completed">
-        icon
-        <img src={"../assets/"} />
+      <nav className="formCompleted">
+        {completed && <img src={complete} alt="Completed Form" width="50" />}
+        {!completed && (
+          <img src={uncomplete} alt="Hasn't Completed Form" width="50" />
+        )}
+        {!completed && <button onClick={sendReminder}>Send Reminder</button>}
+        <br />
+        test
+        <img src={managerPicture} alt="Manager Headshot" />
       </nav>
     </>
   );
