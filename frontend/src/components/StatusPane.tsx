a6e01884d8e66a2f15c140ce4e02eb7b2f32312a
Daniel <99993616+daniel4261@users.noreply.github.com>
2024-02-02 09:13:02 -0800

    Add files via upload

---
+++ frontend/src/components/StatusPane.tsx
@@ -0,0 +1,16 @@
+import "../styles/StatusPane.css";
+
+interface Props {
+  employeePicture: string;
+}
+
+export default function StatusPane({ employeePicture }: Props) {
+  return (
+    <>
+      <nav className="has-form-completed">
+        icon
+        <img src={"../assets/"} />
+      </nav>
+    </>
+  );
+}
