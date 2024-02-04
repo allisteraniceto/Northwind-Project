1250a0c23286626827c61801ce90e6e4e21d41f7
allisteraniceto <allisteraniceto@gmail.com>
2024-02-03 18:51:19 -0800

    styling manager dashboard is better but still needs work

--- backend/obj/Debug/net6.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net6.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07dfa107e8c07793aef0f40fd9fd7ee5edc2adf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3b8817e35b182984ccf076a63e570f01d8db63")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/pages/ManagerPage.tsx
+++ frontend/src/pages/ManagerPage.tsx
@@ -1,21 +1,27 @@
 //Manager Dashboard
 //Employee Dashboard
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
-import "../styles/InteractionsPane.css";
 import EmployeeList from "../components/EmployeeList";
+
+import "../styles/InteractionsPane.css";
+import "../styles/ManagerSection.css";
 
 function ManagerPage() {
   return (
     <>
       <Header dashboard="Manager" />
-      <EmployeeList />
-      <div className='employee-container'>
-        <Link to="/ManagerReviewForm">
-          <button type="button" className='performance-review-button'>
-            Take Me To This Employee's Performance Review
-          </button>
-        </Link>
+      <div className="manager-section">
+        <div className="employee_list-container">
+          <EmployeeList />
+        </div>
+        <div className="employee-container">
+          <Link to="/ManagerReviewForm">
+            <button type="button" className="performance-review-button">
+              Take Me To This Employee's Performance Review
+            </button>
+          </Link>
+        </div>
       </div>
     </>
   );
