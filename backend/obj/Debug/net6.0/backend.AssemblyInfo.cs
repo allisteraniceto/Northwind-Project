0171875e09ebca8745170fe873da9cfb20a7bb45
allisteraniceto <allisteraniceto@gmail.com>
2024-04-05 20:55:00 -0700

    npm i

--- backend/obj/Debug/net6.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net6.0/backend.AssemblyInfo.cs
@@ -14,7 +14,6 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+569d6611a2bed144740f2219f3ecb1490b98acb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
