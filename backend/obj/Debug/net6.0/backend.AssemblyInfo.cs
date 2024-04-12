9527eafc3046d9eb17fa26683ff467ed684ed37d
Mark Kneblik <markkneblik@gmail.com>
2024-04-11 18:56:30 -0700

    syncing artifcat files

--- backend/obj/Debug/net6.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net6.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da841fa3a4b9454174fed1e84857ff2cc9dfdc86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
