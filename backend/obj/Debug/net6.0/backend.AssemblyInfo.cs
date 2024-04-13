fe2476ed15882c5714ef442426c5d32df586c632
allisteraniceto <allisteraniceto@gmail.com>
2024-04-13 00:06:01 -0700

    backend files

--- backend/obj/Debug/net6.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net6.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+051696eaa445fb40d9c48116c1505b9f12be6075")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e706aae1520dba16282d11c9e9f0f5aeb10b06")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
